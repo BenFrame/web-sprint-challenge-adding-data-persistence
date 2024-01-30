423b0bceb674221b5a26a579fb504847242da545
Ben Frame <brewerytattooer@gmail.com>
2024-01-30 12:40:41 -0500

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,13 +1,13 @@
 // build your `/api/projects` router here
 const router = require('express').Router()
-const helpers = require('./model')
+const projects = require('./model')
 
-router.get('/:project_id', (req, res, next) => {
-    helpers.getProjects(req.params.project_id)
-        .then(projects => {
-            res.status(200).json(projects)
-        })
-        .catch(next)
+router.get('/', (req, res, next) => {
+    projects.find()
+    .then(projects => {
+        res.json(projects)
+    })
+    .catch(next)
 })
 
 
--- api/server.js
+++ api/server.js
@@ -7,8 +7,11 @@ const server = express()
 server.use(express.json())
 server.use('/api/project', router)
 
-server.use('*', (req,res) => {
-    res.json({api: 'up'})
-})
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
 
 module.exports = server
