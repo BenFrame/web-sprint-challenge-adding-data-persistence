753707977c4ed02e41c8c2f8c3f8226f73aab686
Ben Frame <brewerytattooer@gmail.com>
2024-01-30 16:13:32 -0500

    all tests passing

--- api/task/model.js
+++ api/task/model.js
@@ -3,11 +3,13 @@ const db = require('../../data/dbConfig')
 
 function getTasks(){
     return db('tasks')
+    .leftJoin('projects as p', 'tasks.project_id', 'p.project_id')
 }
 
 async function postTask(task){
     const [task_id] = await db('tasks').insert(task)
     return getTasks().where({task_id}).first()
+    
 }
 
 function convertTaskBoolean(task){
